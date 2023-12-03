dc9bbc3d0c163ab9ea57c348d74e6de48458b633
Jack Garner <jacksonhgarner@outlook.com>
2023-12-03 14:18:57 -0800

    Day 3

--- app/Main.hs
+++ app/Main.hs
@@ -1,55 +1,92 @@
 module Main where
 
-data Color = Red | Blue | Green
-    deriving (Show)
+-- A sparse representation of the Grid. Why are component and part type
+-- parameters? I'm using `part` as a type parameter because I know I'll want to
+-- map, extend, and fold over the part numbers in the engine map.
+data EngineMap component part = EngineMap
+    { partNumbers :: Map Loc part
+    , components :: Map Loc component
+    }
+    deriving (Show, Generic)
+    deriving (Semigroup, Monoid) via (Generically (EngineMap component part))
 
-data Pull a = Pull {redCubes :: a, blueCubes :: a, greenCubes :: a}
-    deriving (Show, Generic, Functor, Foldable, Traversable)
-    deriving (Semigroup, Monoid) via (Generically (Pull a))
+fromComponent :: (Semigroup part) => Loc -> component -> EngineMap component part
+fromComponent loc component =
+    EngineMap{components = mapOf loc component, partNumbers = mempty}
 
-makePull :: (Monoid a) => Color -> a -> Pull a
-makePull Red numCubes = mempty{redCubes = numCubes}
-makePull Blue numCubes = mempty{blueCubes = numCubes}
-makePull Green numCubes = mempty{greenCubes = numCubes}
+fromPartNumber :: (Semigroup component) => Loc -> part -> EngineMap component part
+fromPartNumber loc part =
+    EngineMap{partNumbers = mapOf loc part, components = mempty}
+
+-- If we're iterating over all of the parts, this represents one step of the
+-- iteration
+data EngineIter component part = EngineIter
+    { lookingAt :: Loc
+    , iterPartNumbers :: Map Loc part
+    , iterComponents :: Map Loc component
+    }
+    deriving (Functor, Foldable, Generic, Show)
+
+instance Filterable (EngineIter component) where
+    catMaybes engine@EngineIter{..} =
+        engine{iterPartNumbers = catMaybes iterPartNumbers}
+
+-- Comonad = map over a structure with an index
+instance Comonad (EngineIter component) where
+    extract EngineIter{..} = iterPartNumbers ! lookingAt
+    duplicate engine@EngineIter{..} =
+        engine{iterPartNumbers = mapWithKey (\k _ -> engine{lookingAt = k}) iterPartNumbers}
+
+mapToIter :: EngineMap component part -> EngineIter component part
+mapToIter EngineMap{..} =
+    EngineIter{lookingAt = mempty, iterPartNumbers = partNumbers, iterComponents = components}
+
+at :: EngineIter component part -> Loc -> Maybe component
+at EngineIter{..} loc = iterComponents !? (loc <> lookingAt)
 
 main :: IO ()
 main = do
-    input <- nonemptyLines
+    input <- getContents'
     print $ partA input
     print $ partB input
 
-partA :: [String] -> Int
-partA = sum . keys . filter isPossible . day2Parser
+parseGrid :: MyParser (EngineMap Char Int)
+parseGrid =
+    fmap (foldFrom @(EngineMap (OnlyOne Char) (OnlyOne Int))) $
+        many $
+            choice
+                [ symbolic '.' $> mempty
+                , fromPartNumber <$> getLoc <*> nat
+                , fromComponent <$> getLoc <*> anyChar
+                ]
 
-partB :: [String] -> Int
-partB = sum . fmap product . day2Parser
+partA :: String -> Int
+partA = partTotal . onlyValidParts . mapToIter . runMyParser parseGrid
 
-day2Parser :: [String] -> Map Int (Pull Int)
-day2Parser = foldMap' (parse parseLine)
+numDigits :: Int -> Int
+numDigits = floor @Double . (+ 1) . logBase 10 . fromIntegral
 
-parseLine :: Parser (Map Int (Pull Int))
-parseLine = do
-    gameId <- "Game" *> int <* ":"
-    groups <- parseGroups
-    let maxes = foldAs @(Pull (Max Int)) groups
-    pure [(gameId, maxes)]
+neighbors :: EngineIter component Int -> [Loc]
+neighbors engine =
+    let size = numDigits $ extract engine
+     in Loc -1 0 : Loc size 0 : [Loc x y | x <- [-1 .. size], y <- [-1, 1]]
 
-parseGroups :: Parser [Pull Int]
-parseGroups = parseGroup `sepBy` ";"
+hasSymbolNeighbor :: EngineIter component Int -> Bool
+hasSymbolNeighbor engine = any (isJust . at engine) $ neighbors engine
 
-parseGroup :: Parser (Pull Int)
-parseGroup = foldTo <$> parseCubes `sepBy` ","
+onlyValidParts :: EngineIter component Int -> EngineIter component (Maybe Int)
+onlyValidParts = extend (fmap extract . justWhen hasSymbolNeighbor)
 
-parseCubes :: Parser (Pull (Sum Int))
-parseCubes = do
-    numCubes <- int
-    cubeColor <-
-        choice
-            [ "red" $> Red
-            , "blue" $> Blue
-            , "green" $> Green
-            ]
-    pure $ makePull cubeColor $ Sum numCubes
+partTotal :: EngineIter component (Maybe Int) -> Int
+partTotal = getSum . foldMap' (maybe 0 Sum)
 
-isPossible :: (Ord a, Num a) => Pull a -> Bool
-isPossible Pull{..} = redCubes <= 12 && greenCubes <= 13 && blueCubes <= 14
+partB :: String -> Int
+partB = sum . fmap product . onlyValidGears . mapToIter . runMyParser parseGrid
+
+gearNeighbors :: EngineIter Char Int -> Map Loc [Int]
+gearNeighbors engine =
+    let gears = fmap (engine.lookingAt <>) . filter ((Just '*' ==) . at engine) $ neighbors engine
+     in foldMap (`mapOf` [extract engine]) gears
+
+onlyValidGears :: EngineIter Char Int -> Map Loc [Int]
+onlyValidGears = filter ((2 ==) . length) . fold . extend gearNeighbors
--- app/Prelude.hs
+++ app/Prelude.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE AllowAmbiguousTypes #-}
+{-# LANGUAGE OverloadedRecordDot #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE NoImplicitPrelude #-}
 {-# OPTIONS_GHC -Wno-orphans #-}
@@ -6,19 +7,53 @@
 module Prelude (module Prelude) where
 
 import BasePrelude as Prelude hiding (filter, span)
+import Control.Applicative
+import Control.Comonad as Prelude
+import Control.Monad.State.Strict as Prelude
 import Data.Coerce as Prelude
 import Data.Foldable as Prelude
 import Data.Functor as Prelude
-import Data.Map.Strict as Prelude (Map, elems, keys)
+import Data.Map.Strict qualified as M
+import Data.Maybe as Prelude (isJust)
 import Data.Monoid as Prelude (Sum (..))
-import Data.Semigroup as Prelude (Max (..))
-import GHC.Exts as Prelude hiding (toList)
+import Data.Semigroup as Prelude (Last (..), Max (..))
+import Data.Vector as Prelude (Vector)
+import Debug.Trace as Prelude
+import GHC.Exts as Prelude hiding (toList, traceEvent)
 import Generics.Deriving as Prelude
 import System.IO as Prelude
-import Text.Trifecta as Prelude
+import Text.Trifecta (anyChar)
+import Text.Trifecta as Prelude hiding (anyChar)
+import Text.Trifecta.Delta
 import Witherable as Prelude
 
-instance (a ~ ()) => IsString (Parser a) where
+traceShowIdWith :: (Show a, Show b) => a -> b -> b
+traceShowIdWith a b = traceShow (a, b) b
+
+deriving instance (Num n) => Num (Last n)
+
+data Loc = Loc {x :: Int, y :: Int}
+    deriving (Show, Generic, Ord, Eq)
+
+instance Semigroup Loc where
+    a <> b = Loc (a.x + b.x) (a.y + b.y)
+
+instance Monoid Loc where
+    mempty = Loc 0 0
+
+newtype MyParser a = MyParser (StateT Int Parser a)
+    deriving (Functor, Applicative, Monad, Parsing, DeltaParsing, CharParsing, Alternative, MonadPlus, MonadState Int)
+
+instance TokenParsing MyParser where
+    someSpace = skipMany ((newline *> modify' (+ 1)) <|> void space)
+
+runMyParser :: MyParser a -> String -> a
+runMyParser (MyParser parser) = parse $ evalStateT parser 0
+
+getLoc :: MyParser Loc
+getLoc = Loc <$> fmap (fromIntegral . column) position <*> get
+
+instance (a ~ ()) => IsString (MyParser a) where
     fromString = void . symbol
 
 nonemptyLines :: IO [String]
@@ -27,11 +62,53 @@ nonemptyLines = filter (not . null) . lines <$> getContents'
 parse :: Parser a -> String -> a
 parse parser = foldResult (error . show) id . parseString parser mempty
 
-int :: Parser Int
+int :: (TokenParsing m, Integral i) => m i
 int = fmap fromInteger integer
+
+nat :: (TokenParsing m, Num i) => m i
+nat = fmap fromInteger natural
+
+anyChar :: (TokenParsing m, Coercible Char c) => m c
+anyChar = coerce <$> token Text.Trifecta.anyChar
 
 foldAs :: forall b a f. (Coercible a b, Coercible b a, Foldable f, Monoid b) => f a -> a
 foldAs = coerce @b @a . foldMap' coerce
 
 foldTo :: forall b a f. (Coercible a b, Foldable f, Monoid a) => f a -> b
 foldTo = coerce . fold
+
+foldFrom :: forall a b f. (Coercible a b, Foldable f, Monoid a) => f a -> b
+foldFrom = coerce . fold
+
+justWhen :: (a -> Bool) -> a -> Maybe a
+justWhen p a = if p a then Just a else Nothing
+
+newtype OnlyOne a = Only a
+    deriving (Show, Functor, Num)
+
+instance Semigroup (OnlyOne a) where
+    _ <> _ = error "Attempt to merge values when only one was expected"
+
+-- This Map is the exact same as the other except I override Semigroup (and
+-- Monoid) to merge the values instead of throwing away the later one. You can
+-- get back the default Map's Semigroup behavior with `Map k (First v)`.
+newtype Map k v = Map (M.Map k v)
+    deriving (Show, Generic, Functor, Foldable, Traversable, Filterable)
+
+instance (Ord k, Semigroup v) => Semigroup (Map k v) where
+    Map a <> Map b = Map $ M.unionWith (<>) a b
+
+instance (Ord k, Semigroup v) => Monoid (Map k v) where
+    mempty = Map mempty
+
+mapOf :: k -> v -> Map k v
+mapOf k = Map . M.singleton k
+
+(!) :: (Ord k) => Map k v -> k -> v
+Map m ! k = m M.! k
+
+(!?) :: (Ord k) => Map k v -> k -> Maybe v
+Map m !? k = m M.!? k
+
+mapWithKey :: (k -> a -> b) -> Map k a -> Map k b
+mapWithKey f (Map m) = Map $ M.mapWithKey f m
